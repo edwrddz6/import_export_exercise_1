57539c90495e9767a9e9c56d5adb81470072575e
Edward Diaz <diazedward24@gmail.com>
2023-11-21 19:15:33 -0800

    Create divide and multiplay files

---
+++ calculator.js
@@ -0,0 +1,10 @@
+import { add } from "./add.js";
+
+const resultAdd = add(5, 3);
+console.log(resultAdd);
+
+import { subtract } from "./subtract.js";
+
+const resultSub = subtract(5, 3);
+console.log(resultSub);
+
